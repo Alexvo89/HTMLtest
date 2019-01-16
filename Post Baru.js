564933eed250bd8527ec684b66de50c058c9b446
Alexvo89 <46750886+Alexvo89@users.noreply.github.com>
2019-01-16 21:00:00 +0700

    Add files via upload

---
+++ Post Baru.js
@@ -0,0 +1,20 @@
+
+<style type="text/css">
+/*Post Baru*/
+#breaking-newsticker{margin:15px 0;position:relative}
+#post_breakingnews{position:relative;z-index:9;overflow:hidden;padding:8px;width:100%!important;box-sizing:border-box;box-shadow:0 5px 5px -5px #3339}
+.breakingnewstitle{float:left;background-color:#7FC7AF;color:#fff;padding:7px 10px;font-weight:bold;border-radius:4px;text-transform:uppercase;position:relative}
+.breakingnewstitle:after{height:0;width:0;position:absolute;content:" ";pointer-events:none;margin-top:1px;border-left:13px solid #7FC7AF;border-top:10px solid transparent;border-bottom:10px solid transparent;-moz-transform:scale(.9999);margin-left:9px;z-index:9}
+.breakingnews marquee{width:80%;position:absolute}
+@media screen and (max-width:800px){.breakingnews marquee{width:70%}}
+@media screen and (max-width:500px){.breakingnews .breakingnewstitle{width:90px;font-size:11px;font-weight:normal}.breakingnews .breakingnewstitle:after{content:none}.breakingnews marquee{width:55%}}
+.breakingnews marquee a{color:#5E5E5E;margin:0 15px;line-height:2}
+#post_breakingnews .breakingnews{width:1120px;max-width:100%;margin:0 auto}
+</style>
+<script type='text/javascript'>
+var url_breaking = "https://chemistryisfun87.blogspot.com";
+var jumlah_post = 5;
+var marquee_speed = "7";
+var close_button = false;
+var info_text = true;
+</script> 
